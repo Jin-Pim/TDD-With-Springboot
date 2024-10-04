367be21d6a4f4a00f0f0dfebd8c7c0a6b1b13b48
Jin-Pim <jintadanai.pim@gmail.com>
2024-10-04 15:56:00 +0700

    tdd

--- src/test/java/com/example/day1/testdouble/GenerateNumberTest.java
+++ src/test/java/com/example/day1/testdouble/GenerateNumberTest.java
@@ -23,4 +23,29 @@ class GenerateNumberTest {
         GenerateNumber g = new GenerateNumber(stubRandom);
         assertEquals(14, g.getNumber());
     }
+
+    @Test
+    @DisplayName("Check nextInt (int bound) called with Spy")
+    void checkCall() {
+
+        SpyRandom spyRandom = new SpyRandom();
+
+        GenerateNumber g = new GenerateNumber(spyRandom);
+        g.getNumber();
+        g.getNumber();
+
+        assertTrue(spyRandom.verify(2));
+    }
+}
+
+class SpyRandom extends Random {
+    int counter = 0;
+    @Override
+    public int nextInt(int bound) {
+        counter++;
+        return 7;
+    }
+    boolean verify(int expected) {
+        return counter == expected;
+    }
 }
